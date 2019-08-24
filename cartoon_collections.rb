8c6c2ab240e7c6eb984e721152193a2ac59e5793
Alfredo Diaz <mail4alfredo@gmail.com>
2019-08-24 15:25:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,6 @@
 def roll_call_dwarves(dwarves)
-  dwarves = 
-  Doc
-  Dopey
-  Bashful
-  Grumpy
-  Sneezy
-  Sleepy
-  Happy
-]
+  dwarves = ["Doc", "Dopey", "Bashful", "Grumpy", "Sneezy", "Sleepy", "Happy" ]
+
   dwarves.each_with_index do |dwarf, index|
     puts "#{index + 1}. #{dwarf}"
   end
